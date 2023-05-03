b934c808af7b2a2e302d410694f91a51339c77d5
Clau4705 <claudiatorres.silvaaa5@gmail.com>
2023-05-03 19:52:37 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# "CONVERSOR DE MOEDAS"
+
+## 📜 Descrição
+Esse projeto foi realizado, com objetivo de mostrar o resultado da conversão de moedas.Foi feito com as skills 
+abaixo:
+
+## ✍ Processo
+- Marcação Semântica HTML5
+- Propriedades personalizadas de CSS3
+- JavaScript
+
+## 📝 A página
+<img src="">
+
+
+## 👩‍💻 Dev's
+<table align="center">
+  <tr>
+    
+
+  <td align="center">
+      <div>
+        <img src="https://avatars.githubusercontent.com/u/117130898?v=4" width="120px;" alt="Foto no GitHub"/><br>
+          <b> Claudinéia Torres </b><br>
+            <a href="https://www.linkedin.com/in/claudineia-torres-00456b239/" alt="Linkedin"><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" height="20"></a>
+            <a href="https://github.com/Clau4705" alt="GitHub"><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" height="20"></a>
+      </div>
+    </td>
+
+  </tr>
+</table>
+
+
---
+++ scripts.js
@@ -0,0 +1,52 @@
+const button = document.getElementById("convert-button")
+const select = document.getElementById("currency-select")
+
+const dolar = 5.2
+const euro = 5.9 
+
+const convertValues = () => {
+const inputReal = document.getElementById("input-real") .value;
+const realValueText = document.getElementById("real-value-text");
+const currencyValueText = document.getElementById("currency-value-text");
+
+realValueText.innerHTML = new Intl.NumberFormat("pt-BR",{
+    style: "currency",
+    currency: "BRL",
+   }).format(inputReal)
+
+    if(select.value === "US$ Dólar americano") {
+  currencyValueText.innerHTML = new Intl.NumberFormat("en-US",{
+     style: "currency",
+     currency: "USD",
+      }).format(inputReal / dolar)     
+    }
+
+   if(select.value === "€ Euro") {
+     currencyValueText.innerHTML = new Intl.NumberFormat("de-De",{
+        style: "currency",
+     currency: "EUR",
+     }).format(inputReal / euro) 
+    }
+
+}
+
+changeCurrency = () => {
+  const currencyName = document.getElementById("currency-name")
+  const currencyImg = document.getElementById("currency-img")
+
+    if (select.value === "US$ Dólar americano") {
+      currencyName.innerHTML = "Dólar americano"
+        currencyImg.src = "../img/estados-unidos (1) 1.svg"
+    }
+                    
+    if(select.value === "€ Euro") {
+      currencyName.innerHTML = "Euro"
+    currencyImg.src = "../img/simbolo-euro.svg"
+    }
+    
+    convertValues ()
+    
+}
+
+button.addEventListener("click", convertValues)
+select.addEventListener("change", changeCurrency)
