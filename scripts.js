09c8af487658660926ef10f6c62448f09495d257
Clau4705 <claudiatorres.silvaaa5@gmail.com>
2023-05-03 21:20:06 -0300

    first commit

--- scripts.js
+++ scripts.js
@@ -2,7 +2,8 @@ const button = document.getElementById("convert-button")
 const select = document.getElementById("currency-select")
 
 const dolar = 5.2
-const euro = 5.9 
+const euro = 5.9
+
 
 const convertValues = () => {
 const inputReal = document.getElementById("input-real") .value;
@@ -27,8 +28,7 @@ realValueText.innerHTML = new Intl.NumberFormat("pt-BR",{
      currency: "EUR",
      }).format(inputReal / euro) 
     }
-
-}
+         }
 
 changeCurrency = () => {
   const currencyName = document.getElementById("currency-name")
@@ -36,13 +36,14 @@ changeCurrency = () => {
 
     if (select.value === "US$ Dólar americano") {
       currencyName.innerHTML = "Dólar americano"
-        currencyImg.src = "../img/estados-unidos (1) 1.svg"
+        currencyImg.src = "./img/estados-unidos (1) 1.svg"
     }
                     
     if(select.value === "€ Euro") {
       currencyName.innerHTML = "Euro"
-    currencyImg.src = "../img/simbolo-euro.svg"
+    currencyImg.src = "./img/simbolo-euro.svg"
     }
+
     
     convertValues ()
     
